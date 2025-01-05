c0dfe48a5bca77da4d30a3754b343fc038fea25e
nazish-parvez <nazishparvez13@gmail.com>
2025-01-05 23:06:21 +0530

    DarL Light Mode

--- Dark Light Mode/src/components/Hero.jsx
+++ Dark Light Mode/src/components/Hero.jsx
@@ -5,7 +5,26 @@ import { ThemeContext } from "../context/ThemeData";
 const Hero = () => {
   const ThemeData = useContext(ThemeContext);
   return (
-    
+    <div className={`flex flex-col justify-center items-center h-screen gap-12 transition-all duration-700 ${
+      ThemeData.theme === "dark" ? "bg-gray-800" : "bg-white"
+    }`}
+  >
+    <h1 className={`text-7xl transition-all duration-700 ${
+        ThemeData.theme === "dark" ? "text-white" : "text-black"
+      }`}
+    >
+      Dark Light Mode Website
+    </h1>
+
+    <p className={`text-5xl transition-all duration-700 ${
+        ThemeData.theme === "dark" ? "text-white" : "text-black"
+      }`}
+    >
+      Hello I am using React JS {ThemeData.theme === "dark" ? }
+    </p>
+
+    <Button />
+  </div>
   );
 };
 
