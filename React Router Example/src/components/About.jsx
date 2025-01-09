4d0b038ec5f632ba24eb75647bfef574a7b63247
nazish-parvez <nazishparvez13@gmail.com>
2025-01-09 18:00:13 +0530

    React Router Examples

---
+++ React Router Example/src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About Jee</div>
+  )
+}
+
+export default About
---
+++ React Router Example/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home Jee</div>
+  )
+}
+
+export default Home
