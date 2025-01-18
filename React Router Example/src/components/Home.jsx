9f6ef239b5893a4b8458ac8a7de64fee1ec89978
nazish-parvez <nazishparvez13@gmail.com>
2025-01-19 01:16:23 +0530

    Updated

--- React Router Example/src/components/Home.jsx
+++ React Router Example/src/components/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home Jee</div>
-  )
-}
+  return <div>Home Component </div>;
+};
 
-export default Home
+export default Home;
