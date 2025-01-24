6eb6419e1efc5a6fc5a0c1e1507850c79f467712
nazish-parvez <nazishparvez13@gmail.com>
2025-01-24 21:58:42 +0530

    Updated

--- React Router Example/src/components/Movies.jsx
+++ React Router Example/src/components/Movies.jsx
@@ -3,10 +3,7 @@ import React from "react";
 const Movies = () => {
   return (
     <main>
-      <h1>Movie Data</h1>
-      <p>Title</p>
-      <p>Body</p>
-      <p>Rating</p>
+     <div>Movies</div>
     </main>
   );
 };
