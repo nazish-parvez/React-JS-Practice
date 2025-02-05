deab28c32dca3654a956449897352a5e5702ebe4
nazish-parvez <nazishparvez13@gmail.com>
2025-02-05 22:28:04 +0530

    Updated

--- React Router Example/src/components/DetailtsPage.jsx
+++ React Router Example/src/components/DetailtsPage.jsx
@@ -8,6 +8,12 @@ const DetailtsPage = () => {
       tenetur? Officiis natus vel, consectetur ratione similique non ex, odio
       placeat nulla exercitationem ab, voluptas rerum. Eveniet, natus deleniti
       accusantium quidem odio distinctio sed deserunt ut corporis amet.
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam
+        consectetur error quos atque assumenda, sint aspernatur reprehenderit
+        veritatis eaque amet minima facilis, dolores fuga excepturi blanditiis,
+        dicta neque? Vero?
+      </p>
     </div>
   );
 };
