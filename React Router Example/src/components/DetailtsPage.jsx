bd76fd6a5490e95f9dbc4889e7eee48b09b9b1b1
nazish-parvez <nazishparvez13@gmail.com>
2025-02-05 22:28:45 +0530

    Updated

--- React Router Example/src/components/DetailtsPage.jsx
+++ React Router Example/src/components/DetailtsPage.jsx
@@ -3,11 +3,6 @@ import React from "react";
 const DetailtsPage = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam obcaecati
-      numquam eaque quis, iusto autem veniam, eos corrupti expedita facere et at
-      tenetur? Officiis natus vel, consectetur ratione similique non ex, odio
-      placeat nulla exercitationem ab, voluptas rerum. Eveniet, natus deleniti
-      accusantium quidem odio distinctio sed deserunt ut corporis amet.
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam
         consectetur error quos atque assumenda, sint aspernatur reprehenderit
