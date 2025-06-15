be9d4819941bf392e73ae6943bb8954a5f19534a
nazish-parvez <nazishparvez13@gmail.com>
2025-06-15 21:59:23 +0530

    Updated

--- Todo Application/src/components/forwardRef/Parent.jsx
+++ Todo Application/src/components/forwardRef/Parent.jsx
@@ -15,7 +15,7 @@ const Parent = () => {
       <form onSubmit={handleFormSubmit}>
         <Child ref={username} inputLabel="Username" />
         <Child ref={password} inputLabel="Password" />
-        <Child ref={password} inputLabel="Password" />
+        
         <div>
           <button>Add Task</button>
         </div>
