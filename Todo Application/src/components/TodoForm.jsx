416484c48ee476fb24796db14d22ccca1e05c54b
nazish-parvez <nazishparvez13@gmail.com>
2025-06-17 13:09:28 +0530

    Updated

--- Todo Application/src/components/TodoForm.jsx
+++ Todo Application/src/components/TodoForm.jsx
@@ -30,7 +30,7 @@ const TodoForm = () => {
         <button type="submit">Remove Task</button>
         <button type="submit">Update Task</button>
         <button type="submit">Update Task</button>
-        <button type="submit">Update Task</button>
+
       </form>
     </>
   );
